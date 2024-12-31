4fc3d843c9e5a33f0dec9c1fe3cbf9ab646b6efa
AtakltiHanis <atakltihanis14@gmail.com>
2024-12-31 15:45:48 +0300

    Starting of project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Github</h1>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stat />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you for your 😍 trip?</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stat() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X itemsl on you list, and you already packed X(x%)</em>
+    </footer>
+  );
+}
