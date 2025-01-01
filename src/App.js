b523b843bcf98e898cd789fe3ddc6584fb57d8ff
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-01 10:33:31 +0300

    Starting PUsh

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "charger", quantity: 13, packed: false },
+  { id: 4, description: "airpod", quantity: 10, packed: true },
+];
 export default function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
       <PackingList />
-      <Stat />
+      <Stats />
     </div>
   );
 }
 
 function Logo() {
-  return <h1>🌴 Far away 💼</h1>;
+  return <h1>🌴 Far Away 💼</h1>;
 }
+
 function Form() {
   return (
     <div className="add-form">
-      <h3>What do you for your 😍 trip?</h3>
+      <h3>What do you need for your 😍 trip?</h3>
     </div>
   );
 }
+
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-function Stat() {
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+    </li>
+  );
+}
+function Stats() {
   return (
     <footer className="stats">
-      <em>👜 You have X itemsl on you list, and you already packed X(x%)</em>
+      <em>👜 You have X items list, and you already packed X(x%)</em>
     </footer>
   );
 }
