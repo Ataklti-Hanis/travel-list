73e2d3a66244dace8016a85d2dd56f66c0e41112
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-03 09:18:25 +0300

    on the way to final

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stat />
+      <Stat items={items} />
     </div>
   );
 }
@@ -103,10 +103,21 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stat() {
+function Stat({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list 🚀!</p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X(x%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `👜 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
