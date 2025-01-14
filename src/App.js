50f085c38d461ff0cc9c60ed3cb5a46db1868f7f
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-14 09:42:31 +0300

    component creation

--- src/App.js
+++ src/App.js
@@ -1,76 +1,32 @@
 import { useState } from "react";
-import Logo from "./Logo";
-import Form from "./Form";
-import PackingList from "./PackingList";
 export default function App() {
-  const [items, setItem] = useState([]);
-  function handleAddItem(item) {
-    setItem((items) => [...items, item]);
-  }
-  function handleDeleteItem(id) {
-    console.log(id);
-    setItem((items) => items.filter((item) => item.id !== id));
-  }
-  function handleToggleItem(id) {
-    setItem((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-  function handleClearList() {
-    const confirmed = window.confirm(
-      "Are You sure you want to delete all item?"
-    );
-    if (confirmed) setItem([]);
-  }
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={handleAddItem} />
-      <PackingList
-        items={items}
-        onDeleteItem={handleDeleteItem}
-        onToggleItem={handleToggleItem}
-        onClearList={handleClearList}
-      />
-      <Stat items={items} />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-function Item({ item, onDeleteItem, onToggleItem }) {
+function Logo() {
+  return <h1>🌳 Far Away 💼</h1>;
+}
+function Form() {
   return (
-    <li>
-      <input
-        type="checkbox"
-        value={item.packed}
-        onChange={() => {
-          onToggleItem(item.id);
-        }}
-      />
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>❌</button>
-    </li>
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
   );
 }
-function Stat({ items }) {
-  if (!items.length)
-    return (
-      <p className="stats">Start adding some items to your packing list 🚀!</p>
-    );
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((numPacked / numItems) * 100);
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
   return (
     <footer className="stats">
-      <em>
-        {percentage === 100
-          ? "You got everything! Ready to go ✈"
-          : `👜 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
-      </em>
+      <em>👜 You have X items on your list, you already packed X(X%)</em>
     </footer>
   );
 }
