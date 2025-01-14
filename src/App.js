d4b67b0ec106ffed1162fbb3fb664ea1acf1776d
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-14 10:01:08 +0300

    added packing list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useState } from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 export default function App() {
   return (
     <div className="app">
@@ -21,7 +25,18 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+function Item({ item }) {
+  return <li className="">{item.description}</li>;
 }
 function Stats() {
   return (
